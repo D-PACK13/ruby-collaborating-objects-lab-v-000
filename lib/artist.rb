204ad43eef4fddefc1348da69df3696e55b0653a
Doug Lokrantz <doug.lokrantz@gmail.com>
2018-05-30 14:27:46 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,6 +9,20 @@ class Artist
     @@all << self
   end
   
+    def self.create_by_name(name)
+  	@name = name
+	  @@all << self.new(@name)
+  	self.all.detect{|song| song.name == name}
+  end 
   
+  def self.find_by_name(name)
+  	@name = name
+  	self.all.detect{|song| song.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+
   
 end
